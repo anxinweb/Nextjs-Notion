7d41b62254fe6f0900d2108da518f94a329cfbe4
fangs <2055787352@qq.com>
2024-06-03 21:34:05 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -11,7 +11,7 @@ export default siteConfig({
   // basic site info (required)
   name: 'Ah新的生活小札',
   domain: 'https://blog.2xn.top',
-  author: 'ah新,
+  author: 'ah新',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
